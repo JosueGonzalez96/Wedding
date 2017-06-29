f7c67b17c44d84c9274a7a1401599bf6b3faeac5
Gesfor <gesfor@MacBook-Air-de-Gesfor-2.local>
2017-06-29 15:37:51 -0500

    Initial Commit

---
+++ wedding/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  wedding
+//
+//  Created by Gesfor on 29/06/17.
+//  Copyright © 2017 Alberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ wedding/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  wedding
+//
+//  Created by Gesfor on 29/06/17.
+//  Copyright © 2017 Alberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
